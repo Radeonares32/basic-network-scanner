9128c2f7f3601c3044b31695eebfe07d18b3e451
Radeonares32 <bugra.linux.js@gmail.com>
2022-07-14 13:55:16 +0300

    update scanner

--- network_scanner.py
+++ network_scanner.py
@@ -1,16 +1,30 @@
+from click import argument
 import scapy.all as scapy
-
-arp_request_packet = scapy.ARP(pdst="192.168.8.1/24")
-
-broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+import optparse as parse
 
 
-combined_packet = broadcast_packet/arp_request_packet
+def user_input():
+    parse_object = parse.OptionParser()
+    parse_object.add_option("-i", "--ipaddress",
+                            dest="ip_address_varrib", help="Enter IP Address")
+    (user_inp, arguments) = parse_object.parse_args()
 
-(answer,unaswered) =scapy.srp(combined_packet,timeout=1)
+    if not user_inp.ip_address_varrib:
+        print("Enter IP Address")
+    return user_inp
+def scan(ip):
+    arp_request_packet = scapy.ARP(pdst=ip)
 
-answer.summary()
+    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 
+    combined_packet = broadcast_packet/arp_request_packet
 
-#scapy.ls(scapy.Ether())
-#scapy.ls(scapy.ARP())
+    (answer, unaswered) = scapy.srp(combined_packet, timeout=1)
+
+    answer.summary()
+
+user_ip_address = user_input()
+scan(user_ip_address.ip_address_varrib)
+
+# scapy.ls(scapy.Ether())
+# scapy.ls(scapy.ARP())
